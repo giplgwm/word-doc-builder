96c3f8035e123aee606d9a63f4caeae7ef6ed0ac
gip <98342753+giplgwm@users.noreply.github.com>
2024-10-16 10:59:52 -0400

    Update image_processing.py

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -2,20 +2,16 @@ from PIL import Image
 
 def resize_image(image_path, target_width, target_height):
     with Image.open(image_path) as img:
-        # Calculate the aspect ratio
         img_ratio = img.width / img.height
         target_ratio = target_width / target_height
 
         if img_ratio > target_ratio:
-            # Image is wider, so we'll resize based on width
             new_width = target_width
             new_height = int(new_width / img_ratio)
         else:
-            # Image is taller, so we'll resize based on height
             new_height = target_height
             new_width = int(new_height * img_ratio)
 
-        # Resize the image
         img_resized = img.resize((new_width, new_height), Image.LANCZOS)
 
         return img_resized
