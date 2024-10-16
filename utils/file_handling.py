2a2ae67eb49080a262de09fe0d4fb60d5013541b
gip <98342753+giplgwm@users.noreply.github.com>
2024-10-16 10:59:34 -0400

    Update file_handling.py

--- utils/file_handling.py
+++ utils/file_handling.py
@@ -11,7 +11,6 @@ def save_uploaded_file(uploaded_file, filename=None):
     Save the uploaded file to a permanent directory, rotate it according to EXIF data,
     and return the file path and MD5 hash.
     """
-    # Calculate MD5 hash of the file content
     if isinstance(uploaded_file, io.BytesIO):
         file_content = uploaded_file.getvalue()
     else:
@@ -20,10 +19,8 @@ def save_uploaded_file(uploaded_file, filename=None):
     
     md5_hash = hashlib.md5(file_content).hexdigest()
 
-    # Create a permanent directory if it doesn't exist
     os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-    # Determine file extension
     if filename:
         file_extension = os.path.splitext(filename)[1].lower()
     elif hasattr(uploaded_file, 'name'):
@@ -31,18 +28,13 @@ def save_uploaded_file(uploaded_file, filename=None):
     else:
         file_extension = '.jpg'  # Default to .jpg if no extension is available
 
-    # Generate a unique filename using the MD5 hash
     unique_filename = f"{md5_hash}{file_extension}"
 
-    # Save the file
     file_path = os.path.join(UPLOAD_DIR, unique_filename)
 
-    # Open the image using PIL
+    # Rotate the image according to EXIF data. This fixes the bug of photos not being oriented properly
     with Image.open(uploaded_file) as img:
-        # Rotate the image according to EXIF data
         img_rotated = ImageOps.exif_transpose(img)
-
-        # Save the rotated image
         img_rotated.save(file_path)
 
     print(f"File saved: {file_path}")
