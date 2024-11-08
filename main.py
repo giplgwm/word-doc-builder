a00870a3b4b3803788f994adb16de5896f3f1cb5
gip <98342753+giplgwm@users.noreply.github.com>
2024-11-08 12:47:08 -0500

    Add selected counter and deselect all button

--- main.py
+++ main.py
@@ -19,10 +19,11 @@ if 'blocked_photos' not in st.session_state:
 if 'file_uploader_key' not in st.session_state:
     st.session_state.file_uploader_key = 1
 
-uploaded_files = st.file_uploader("Upload photos or email files (.msg or .eml)",
-                                  type=["jpg", "jpeg", "png", "msg", "eml"],
-                                  accept_multiple_files=True,
-                                 key=f'fileuploader_{st.session_state.file_uploader_key}')
+uploaded_files = st.file_uploader(
+    "Upload photos or email files (.msg or .eml)",
+    type=["jpg", "jpeg", "png", "msg", "eml"],
+    accept_multiple_files=True,
+    key=f'fileuploader_{st.session_state.file_uploader_key}')
 if uploaded_files:
     new_files_added = False
     for uploaded_file in uploaded_files:
@@ -37,13 +38,14 @@ if uploaded_files:
             extracted_images = [(uploaded_file, uploaded_file.name)]
 
         for img_data, img_filename in extracted_images:
-            file_path, md5_hash = save_uploaded_file(img_data, filename=img_filename)
-            
+            file_path, md5_hash = save_uploaded_file(img_data,
+                                                     filename=img_filename)
+
             # Check if the file is not in the blocked list and not already in the session state
             if md5_hash not in st.session_state.blocked_photos and not any(
                     photo.get('md5_hash') == md5_hash
                     for photo in st.session_state.photos):
-                
+
                 st.session_state.photos.append({
                     "path": file_path,
                     "md5_hash": md5_hash,
@@ -65,8 +67,23 @@ if st.session_state.photos:
 if st.session_state.photos:
     st.sidebar.title("Photo Controls")
     st.sidebar.markdown("---")
-    st.sidebar.write("Select images and then use the controls below to reorder or remove them")
+    st.sidebar.write(
+        "Select images and then use the controls below to reorder or remove them"
+    )
     st.sidebar.markdown("---")
+    st.sidebar.write(
+        f"Photos Selected: {len([photo for photo in st.session_state.photos if photo['selected']])}"
+    )
+
+    if st.sidebar.button('Deselect All'):
+        selected = [
+            photo for photo in st.session_state.photos if photo['selected']
+        ]
+        for photo in selected:
+            photo['selected'] = False
+        st.rerun()
+    st.sidebar.markdown("---")
+
     if st.sidebar.button("Move Up"):
         selected_indices = [
             i for i, photo in enumerate(st.session_state.photos)
@@ -100,31 +117,39 @@ if st.session_state.photos:
         st.rerun()
 
     if st.sidebar.button('Remove Selected'):
-        selected_hashes = [photo['md5_hash'] for photo in st.session_state.photos if photo['selected']]
-        
-        st.session_state.blocked_photos.extend(selected_hashes)
-        
-        st.session_state.photos = [photo for photo in st.session_state.photos if not photo['selected']]
-        
-        
-        st.rerun()
+        selected_hashes = [
+            photo['md5_hash'] for photo in st.session_state.photos
+            if photo['selected']
+        ]
 
+        st.session_state.blocked_photos.extend(selected_hashes)
+
+        st.session_state.photos = [
+            photo for photo in st.session_state.photos if not photo['selected']
+        ]
+
+        st.rerun()
 
     for i, photo in enumerate(st.session_state.photos):
         with st.container():
-            is_selected = st.checkbox("Select", key=f"select_{i}", value=photo["selected"])
+            is_selected = st.checkbox("Select",
+                                      key=f"select_{i}",
+                                      value=photo["selected"])
             if is_selected != photo["selected"]:
                 st.session_state.photos[i]["selected"] = is_selected
                 st.rerun()
 
             display_width = 250
-            st.image(photo["path"], use_column_width=False, width=display_width)
+            st.image(photo["path"],
+                     use_column_width=False,
+                     width=display_width)
 
-            new_label = st.text_input(f"Label for photo {i+1}", value=photo["label"], key=f"label_{i}")
+            new_label = st.text_input(f"Label for photo {i+1}",
+                                      value=photo["label"],
+                                      key=f"label_{i}")
             st.session_state.photos[i]["label"] = new_label
 
-        st.markdown("---") 
-
+        st.markdown("---")
 
 if st.session_state.photos:
     document_name = st.text_input("Enter document name")
